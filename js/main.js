11d851d24b0350335ffa6c0b79094ebc61b9c908
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2017-11-02 11:48:54 -0700

    added google analytics

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+	window.dataLayer = window.dataLayer || [];
+
+	function gtag() {
+		dataLayer.push(arguments);
+	}
+	gtag('js', new Date());
+
+	gtag('config', 'UA-108344152-3');
+
+});
