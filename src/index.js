e45624a79beabcc1948933e19965b7b4e30edb4b
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2018-03-15 14:11:22 -0700

    Restructued SASS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'index.scss';
 import App from './scenes/base/index';
 import registerServiceWorker from './registerServiceWorker';
+import 'index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
--- src/scenes/base/index.js
+++ src/scenes/base/index.js
@@ -13,6 +13,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <h1>Forte</h1>
+          <p>Test</p>
+        </div>
       </div>
     );
   }
