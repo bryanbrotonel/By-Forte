a78b358122f509dc69c62d977ba62ad458f076c1
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2018-03-25 22:09:19 -0700

    Updated fonts

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,12 +4,22 @@ import './footer.css';
 
 export class Footer extends React.Component {
   render() {
-    return(
+    return (
       <div className="footer">
         <div className="container">
-          <p>Footer</p>
+          <div class="row">
+            <div class="col-sm">
+              <h2>By Forte</h2>
+            </div>
+            <div class="col-sm">
+              <h2>Links</h2>
+            </div>
+            <div class="col-sm">
+              <p>Test</p>
+            </div>
+          </div>
         </div>
-      </div>
-    )
+    </div>
+  )
   }
 }
