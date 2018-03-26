8d1ccc437a827abdde4d604149d5db2562731d90
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2018-03-25 23:07:33 -0700

    Scaled logo and spacing

--- src/scenes/about/about.js
+++ src/scenes/about/about.js
@@ -3,11 +3,11 @@ import './about.css';
 
 export class About extends Component {
   render() {
-    return (
-      <div className="container">
-        <h1>ABOUT</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget diam finibus, consectetur est vitae, porttitor sapien</p>
-      </div>
-    )
+    return (<div className="container">
+      <h1>ABOUT</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel magna nec turpis tempor vehicula eu et leo. Nulla porta, quam in mollis aliquet, urna sapien pharetra tellus, ut scelerisque metus erat congue metus. Curabitur nec lacus accumsan, lobortis sapien et, elementum nunc. Curabitur vel purus sed nisi malesuada egestas.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel magna nec turpis tempor vehicula eu et leo. Nulla porta, quam in mollis aliquet, urna sapien pharetra tellus, ut scelerisque metus erat congue metus. Curabitur nec lacus accumsan, lobortis sapien et, elementum nunc. Curabitur vel purus sed nisi malesuada egestas.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel magna nec turpis tempor vehicula eu et leo. Nulla porta, quam in mollis aliquet, urna sapien pharetra tellus, ut scelerisque metus erat congue metus. Curabitur nec lacus accumsan, lobortis sapien et, elementum nunc. Curabitur vel purus sed nisi malesuada egestas.</p>
+    </div>)
   }
 }
