90692c038b6c4c6f7dbb7939f49d688a8ea80b43
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2018-05-08 21:00:24 -0700

    Updated picture row format

--- src/components/picture-row.js
+++ src/components/picture-row.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
 import placeholderOne from "images/PlaceholderOne.png";
 import placeholderTwo from "images/PlaceholderTwo.png";
-import placeholderThree from "images/PlaceholderThree.png";
-import logo from 'images/By Forte Primary (Black).png';
+import placeholderThree from "images/PlaceholderThree.jpg";
 
 export class PictureRow extends Component {
     render() {
         return (
             <div className="d-flex justify-content-around w-100 flex-column flex-lg-row">
-                <div className="p-2 picture"
+                <div className="picture"
                     style={
                         {
                             backgroundImage: 'url(' + placeholderOne + ')'
                         }
                     }>
                 </div>
-                <div className="p-2 picture"
+                <div className="picture"
                     style={
                         {
                             backgroundImage: 'url(' + placeholderTwo + ')'
                         }
                     }>
                 </div>
-                <div className="p-2 picture"
+                <div className="picture"
                     style={
                         {
                             backgroundImage: 'url(' + placeholderThree + ')'
