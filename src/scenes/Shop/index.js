95f1de012ed9db357f4b7b681c4e76dea78a8155
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2018-05-16 21:13:05 -0700

    Redid folder structure

--- src/scenes/Shop/index.js
+++ src/scenes/Shop/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { ComingSoon } from '../components/coming-soon';
-import 'scss/scenes/shop.css';
+import {ComingSoon} from '../../components/ComingSoon';
+import './styles.css';
 
 export class Shop extends Component {
   render() {
