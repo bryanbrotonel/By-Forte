21033e3ff0b6e8a9dcb00aee037cb03255866312
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2018-05-16 21:54:56 -0700

    Revert "Revert "Redid folder structure""

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from "react-router-dom";
-import App from 'scenes/app';
+import App from 'scenes/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'scss/main.css';
