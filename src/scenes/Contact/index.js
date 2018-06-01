8638bc15d3b8b305f00e6d4c1b2cbec4b78dabed
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2018-05-31 23:23:05 -0700

    Added contact info

--- src/scenes/Contact/index.js
+++ src/scenes/Contact/index.js
@@ -34,87 +34,40 @@ export class Contact extends Component {
   }
 
   render() {
-    var textareaStyle = {
-      resize: "none"
-    };
-
     return (
       <div className="container">
-        <div className="col-12 col-md-6 uk-card uk-card-default uk-card-body mx-auto">
-          <h2 className="text-dark">CONTACT</h2>
-          <br />
-          <form onSubmit={this.handleSubmit}>
-            <div className="form-row">
-              <div className="form-group col-md-6">
-                <label htmlFor="firstName">First Name</label>
-                <input
-                  id="firstName"
-                  name="firstName"
-                  type="text"
-                  className="uk-input"
-                  value={this.state.firstName}
-                  onChange={this.handleFormChange}
-                  required
-                />
-              </div>
-              <div className="form-group col-md-6">
-                <label htmlFor="firstName">Last Name</label>
-                <input
-                  id="lastName"
-                  name="lastName"
-                  type="text"
-                  className="uk-input"
-                  value={this.state.lastName}
-                  onChange={this.handleFormChange}
-                  required
-                />
-              </div>
-            </div>
-            <div className="form-group">
-              <label htmlFor="Email">Email</label>
-              <input
-                id="email"
-                name="email"
-                type="email"
-                className="uk-input"
-                value={this.state.email}
-                onChange={this.handleFormChange}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="Subject">Subject</label>
-              <input
-                id="subject"
-                name="subtject"
-                type="text"
-                className="uk-input"
-                value={this.state.subject}
-                onChange={this.handleFormChange}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="Email">Message</label>
-              <textarea
-                name="message"
-                id="message"
-                rows="5"
-                className="uk-textarea"
-                style={textareaStyle}
-                value={this.state.message}
-                onChange={this.handleFormChange}
-              />
-            </div>
-            <div className="align-items-right d-flex flex-row-reverse">
-              <input
-                type="submit"
-                className="uk-button uk-button-default uk-form-width-small text-center"
-                value="SUBMIT"
-              />
-            </div>
-          </form>
-        </div>
+        <h1>CONTACT</h1>
+        <br />
+        <h5>
+          <b>EMAIL</b>
+        </h5>
+        <a
+          href="mailto:supplybyforte@gmail.com"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <p>supplybyforte@gmail.com</p>
+        </a>
+        <h5>
+          <b>INSTAGRAM</b>
+        </h5>
+        <a
+          href="https://www.instagram.com/supplybyforte/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <p>@supplybyforte</p>
+        </a>
+        <h5>
+          <b>FACEBOOK</b>
+        </h5>
+        <a
+          href="https://www.facebook.com/supplybyforte"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <p>Supply By Forte</p>
+        </a>
       </div>
     );
   }
