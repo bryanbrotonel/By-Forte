c5aaa1ca4bd89863f763d2194b62f436b43e35e8
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2018-09-25 14:48:21 -0700

    Updated about page

--- src/scenes/About/index.js
+++ src/scenes/About/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import "./styles.scss";
+import "./styles.css";
 
 export default class About extends Component {
   constructor() {
@@ -14,17 +14,25 @@ export default class About extends Component {
 
   render() {
     return (
-      <div className="container">
-        <h1>About</h1>
-        <br />
-        <div className="content">
-          <p>By Forte is a streetwear brand based in Vancouver, Canada.</p>
-          <p>
-            We represent what it means to bring the ideas that we visualize in
-            our minds to life. By Forte embraces the creative process by
-            creating in silence and let our products speak for themselves.
-          </p>
-          <p>Prosper Through Noise</p>
+      <div className="container hv-center">
+        <div className="row justify-content-around">
+          <div className="col-10 col-md-6">
+            <img
+              src="https://source.unsplash.com/rc3So-nVtsw/1600x1024"
+              alt="By Forte About"
+            />
+          </div>
+          <div className="col-10 col-md-5 hv-center">
+            <div className="about-write-up">
+              <p>By Forte is a streetwear from based in Vancouver, Canada.</p>
+              <p>
+                We represent what it means to bring the ideas that we visualize
+                in our minds to life. By Forte embraces the creative process by
+                creating in silence and let our products speak for themselves.
+              </p>
+              <p>Prosper Through Noise</p>
+            </div>
+          </div>
         </div>
       </div>
     );
