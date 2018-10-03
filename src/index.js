a40588e6c4f0c64ed9ad53320e259cd975b9a9d2
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2018-10-03 16:23:11 -0700

    Web app touch ups

--- src/index.js
+++ src/index.js
@@ -16,14 +16,25 @@ import "scss/main.css";
 // Add social media icons
 library.add(faBars, faTimes);
 
+// // Initialize Firebase
+// let config = {
+//   apiKey: "AIzaSyDQC55nWiuHoR2uChUafiFWaYGVH2ecPvI",
+//   authDomain: "by-forte.firebaseapp.com",
+//   databaseURL: "https://by-forte.firebaseio.com",
+//   projectId: "by-forte",
+//   storageBucket: "by-forte.appspot.com",
+//   messagingSenderId: "622359278063"
+// };
+// firebase.initializeApp(config);
+
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDQC55nWiuHoR2uChUafiFWaYGVH2ecPvI",
-  authDomain: "by-forte.firebaseapp.com",
-  databaseURL: "https://by-forte.firebaseio.com",
-  projectId: "by-forte",
-  storageBucket: "by-forte.appspot.com",
-  messagingSenderId: "622359278063"
+  apiKey: "AIzaSyCtWTweCtNXjW_WjYfW59T9IG-upFejw2k",
+  authDomain: "by-forte-test.firebaseapp.com",
+  databaseURL: "https://by-forte-test.firebaseio.com",
+  projectId: "by-forte-test",
+  storageBucket: "by-forte-test.appspot.com",
+  messagingSenderId: "652878859375"
 };
 firebase.initializeApp(config);
 
