88b85e483e6ef15abb5dc60b827bbb111ac82819
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2018-10-14 18:08:57 -0700

    Revert "Delivery 002 Ready"

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import Loadable from "react-loadable";
 
-import Loading from "../../components/Loading";
+import Loading from "./../../components/Loading";
+
 import "./styles.css";
 
-import MP4video from "../../videos/Storm Clouds.mp4";
-
-const VideoBackground = Loadable({
-  loader: () => import("../../components/VideoBackgorund"),
+const ComingSoon = Loadable({
+  loader: () => import("../../components/ComingSoon"),
   loading: Loading
 });
 
@@ -23,11 +22,10 @@ export default class Home extends Component {
 
   render() {
     return (
-      // <ComingSoon
-      //   bgImage="https://source.unsplash.com/K61C1XrwTWs/1600x1024"
-      //   text="Coming Soon"
-      // />
-      <VideoBackground MP4video={MP4video} />
+      <ComingSoon
+        bgImage="https://source.unsplash.com/K61C1XrwTWs/1600x1024"
+        text="Coming Soon"
+      />
     );
   }
 }
