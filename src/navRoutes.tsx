0304b9017432a6b02ac0e6adc7af43606e1fce75
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2022-09-08 15:26:54 -0700

    Added code splitting

--- src/navRoutes.tsx
+++ src/navRoutes.tsx
@@ -1,74 +1,27 @@
 import React, { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './scenes/Home';
 import Loading from './components/Loading';
-import About from './scenes/About';
-import Editorial from './scenes/Editorial';
 import NotFound from './components/NotFound';
-import Shop from './scenes/Shop';
-import ProductPage from './scenes/ProductPage';
-import Checkout from './scenes/Checkout';
+
+const Home = lazy(() => import('./scenes/Home'));
+const About = lazy(() => import('./scenes/About'));
+const Editorial = lazy(() => import('./scenes/Editorial'));
+const Shop = lazy(() => import('./scenes/Shop'));
+const ProductPage = lazy(() => import('./scenes/ProductPage'));
+const Checkout = lazy(() => import('./scenes/Checkout'));
 
 const NavRoutes = () => (
-  <Routes>
-    <Route
-      path="/"
-      element={
-        <Suspense fallback={<Loading />}>
-          <Home />
-        </Suspense>
-      }
-    ></Route>
-    <Route
-      path="/home"
-      element={
-        <Suspense fallback={<Loading />}>
-          <Home />
-        </Suspense>
-      }
-    ></Route>
-    <Route
-      path="/about"
-      element={
-        <Suspense fallback={<Loading />}>
-          <About />
-        </Suspense>
-      }
-    ></Route>
-    <Route
-      path="/editorial"
-      element={
-        <Suspense fallback={<Loading />}>
-          <Editorial />
-        </Suspense>
-      }
-    ></Route>
-    <Route
-      path="/shop"
-      element={
-        <Suspense fallback={<Loading />}>
-          <Shop />
-        </Suspense>
-      }
-    ></Route>
-    <Route
-      path="/shop/:id"
-      element={
-        <Suspense fallback={<Loading />}>
-          <ProductPage />
-        </Suspense>
-      }
-    ></Route>
-    <Route
-      path="/checkout"
-      element={
-        <Suspense fallback={<Loading />}>
-          <Checkout />
-        </Suspense>
-      }
-    ></Route>
-    <Route path="*" element={<NotFound />} />
-  </Routes>
+  <Suspense fallback={<Loading />}>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/editorial" element={<Editorial />}></Route>
+      <Route path="/shop" element={<Shop />}></Route>
+      <Route path="/shop/:id" element={<ProductPage />}></Route>
+      <Route path="/checkout" element={<Checkout />}></Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </Suspense>
 );
 
 export default NavRoutes;
