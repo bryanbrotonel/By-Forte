6494d0b6c69d46c33fe563e6811b9e50908e518b
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2022-09-08 15:21:00 -0700

    Added jest testing

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)(?:\\?as=webp)?$':
+      '<rootDir>/src/__mocks__/fileMock.ts',
+    '\\.(css|less)$': '<rootDir>/src/__mocks__/styleMock.ts',
+  },
+};
---
+++ src/__tests__/sampleTest.tsx
@@ -0,0 +1,11 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { expect } from '@jest/globals';
+import { act, render } from '@testing-library/react';
+
+test('test', async () => {
+  expect(true);
+});
